af1e6dbcd935e6c96d2f4c04d826fc3b6d3574a4
Harsh chaudhary <146811538+piyush1740x@users.noreply.github.com>
2025-03-03 03:38:57 +0530

    ui change

--- src/app/page.js
+++ src/app/page.js
@@ -25,6 +25,7 @@ export default function Home() {
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ Intrest: interest }),
             });
+
             const data = await res.json();
             if (res.ok) {
                 setCareerIdea(data.career);
@@ -53,7 +54,7 @@ export default function Home() {
                         <input
                             type="text"
                             className="w-full p-3 pl-12 bg-white/20 border border-white/30 rounded-lg text-white placeholder-gray-300 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                            placeholder="Enter your intrest (e.g., Coding, Music, Design...)"
+                            placeholder="Apka interest kya hai? (e.g., Coding, Music, Design...)"
                             value={interest}
                             onChange={(e) => setInterest(e.target.value)}
                         />
@@ -65,15 +66,15 @@ export default function Home() {
                         disabled={loading}
                         className="mt-4 w-full px-4 py-3 text-lg font-medium bg-blue-500 hover:bg-blue-600 transition rounded-lg disabled:opacity-50 disabled:cursor-not-allowed"
                     >
-                        {loading ? "⏳ Processing..." : "🔍 Get Idea.."}
+                        {loading ? "⏳ Finding" : "🔍 Get Idea.."}
                     </button>
                 </form>
 
                 {error && <p className="mt-4 text-red-400 text-center text-lg">⚠️ {error}</p>}
 
                 {careerIdea && (
-                    <div className="mt-6 p-4 bg-white/20 border border-white/30 rounded-lg shadow-md">
-                        <h2 className="text-2xl font-bold mb-2">✨ Career Ideas for You</h2>
+                    <div className="mt-6 rounded-lg shadow-md">
+                        <h2 className="text-2xl font-bold mb-3 bg-green-400 rounded px-3 py-2 ">✨ Career Ideas for You</h2>
                         <p className="text-gray-200 whitespace-pre-line text-lg">{careerIdea}</p>
                     </div>
                 )}
